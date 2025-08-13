29e62c084c0bb8cead2abb7621651e80d9f6fba9
VladimirPuzanov <>
2025-08-13 06:00:12 +0300

    Футер готов

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,9 +1,29 @@
+//Зафиксировать тело(да-да, вкусы специфичны, знаю)
+function lockBody(){
+	if(!document.querySelector("body")?.classList.contains("lock")){
+		document.querySelector("body")?.classList.add("lock")
+	}
+}
+
+function unlockBody(){
+	if(document.querySelector("body")?.classList.contains("lock")){
+		document.querySelector("body")?.classList.remove("lock")
+	}
+}
+
+//Флаг фиксации тела
+let bodyIsLock = false
+
+//Управление бургерами(Звучит как новое место работы Якова)
 const burger = document.querySelector(".header__burger")
 const headerMenu = document.querySelector(".header__menu")
-const body = document.querySelector("body")
 if(burger && headerMenu && !window.matchMedia("(min-width: 1024px)").matches){
 	burger.addEventListener("click", ()=>{
+		if(headerMenu.classList.contains("active")){
+			unlockBody()
+		} else{
+			lockBody()
+		}
 		headerMenu.classList.toggle("active")
-		body.classList.toggle("lock")
 	})
 }
