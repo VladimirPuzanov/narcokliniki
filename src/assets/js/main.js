9bd5eb358bb992755955eee6db372d2266d65526
VladimirPuzanov <>
2025-08-13 14:09:00 +0300

    Шаги доделать надо и УСКОРИТЬСЯ ЁБАНА В РОТ!

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -27,3 +27,59 @@ if(burger && headerMenu && !window.matchMedia("(min-width: 1024px)").matches){
 		headerMenu.classList.toggle("active")
 	})
 }
+
+//Анимации
+function formatNumber(num) {
+  return String(num)
+    .replace(/\D/g, '') 
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+}
+
+function updateCounter(element, stop, increment){
+	if(parseInt(element.innerText) + increment > stop){
+		element.innerText = formatNumber(stop)
+	} else {
+		setTimeout(() => {
+			element.innerText = (parseInt(element.innerText) + increment)
+			updateCounter(element, stop, increment)
+		}, 1);
+	}
+}
+
+function counterAnimate(element, start, stop, time){
+	element.innerText = start;
+	increment = Math.trunc((stop - start) / time)
+	updateCounter(element, stop, increment)
+}
+
+const observer = new IntersectionObserver((entries) => {
+  let animationDelay = 0;
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+			if(entry.target.classList.contains("counter")){
+				stop = parseInt(entry.target.innerText.replaceAll(" ", ""))
+				counterAnimate(entry.target, 0, stop, 200)
+			}
+      if (entry.target.classList.contains("animation-group")) {
+        animationDelay = animationDelay + 100
+      } else if(entry.target.closest("animation-group")?.classList.contains("animated")){
+        animationDelay = 0;
+      }
+      setTimeout(() => {
+        entry.target.classList.add("animated")
+      }, animationDelay);
+      observer.unobserve(entry.target)
+    }
+  });
+}, {
+  threshold: 0.3,
+  rootMargin: '50px',
+});
+
+const animatedItems = document.querySelectorAll(".to_animate")
+
+if (animatedItems.length > 0) {
+  animatedItems.forEach(item => {
+    observer.observe(item)
+  })
+}
