39240c599cc8d845f0bfb9f8b09fcdfaa7facbfa
VladimirPuzanov <volodya938@yandex.ru>
2025-08-14 14:04:31 +0300

    Доделать бы главную сегодня

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,12 +1,12 @@
 //Зафиксировать тело(да-да, вкусы специфичны, знаю)
-function lockBody(){
-	if(!document.querySelector("body")?.classList.contains("lock")){
+function lockBody() {
+	if (!document.querySelector("body")?.classList.contains("lock")) {
 		document.querySelector("body")?.classList.add("lock")
 	}
 }
 
-function unlockBody(){
-	if(document.querySelector("body")?.classList.contains("lock")){
+function unlockBody() {
+	if (document.querySelector("body")?.classList.contains("lock")) {
 		document.querySelector("body")?.classList.remove("lock")
 	}
 }
@@ -17,11 +17,11 @@ let bodyIsLock = false
 //Управление бургерами(Звучит как новое место работы Якова)
 const burger = document.querySelector(".header__burger")
 const headerMenu = document.querySelector(".header__menu")
-if(burger && headerMenu && !window.matchMedia("(min-width: 1024px)").matches){
-	burger.addEventListener("click", ()=>{
-		if(headerMenu.classList.contains("active")){
+if (burger && headerMenu && !window.matchMedia("(min-width: 1024px)").matches) {
+	burger.addEventListener("click", () => {
+		if (headerMenu.classList.contains("active")) {
 			unlockBody()
-		} else{
+		} else {
 			lockBody()
 		}
 		headerMenu.classList.toggle("active")
@@ -30,13 +30,13 @@ if(burger && headerMenu && !window.matchMedia("(min-width: 1024px)").matches){
 
 //Анимации
 function formatNumber(num) {
-  return String(num)
-    .replace(/\D/g, '') 
-    .replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+	return String(num)
+		.replace(/\D/g, '')
+		.replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 }
 
-function updateCounter(element, stop, increment){
-	if(parseInt(element.innerText) + increment > stop){
+function updateCounter(element, stop, increment) {
+	if (parseInt(element.innerText) + increment > stop) {
 		element.innerText = formatNumber(stop)
 	} else {
 		setTimeout(() => {
@@ -46,40 +46,59 @@ function updateCounter(element, stop, increment){
 	}
 }
 
-function counterAnimate(element, start, stop, time){
+function counterAnimate(element, start, stop, time) {
 	element.innerText = start;
 	increment = Math.trunc((stop - start) / time)
 	updateCounter(element, stop, increment)
 }
 
 const observer = new IntersectionObserver((entries) => {
-  let animationDelay = 0;
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-			if(entry.target.classList.contains("counter")){
+	let animationDelay = 0;
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			if (entry.target.classList.contains("counter")) {
 				stop = parseInt(entry.target.innerText.replaceAll(" ", ""))
 				counterAnimate(entry.target, 0, stop, 200)
 			}
-      if (entry.target.classList.contains("animation-group")) {
-        animationDelay = animationDelay + 100
-      } else if(entry.target.closest("animation-group")?.classList.contains("animated")){
-        animationDelay = 0;
-      }
-      setTimeout(() => {
-        entry.target.classList.add("animated")
-      }, animationDelay);
-      observer.unobserve(entry.target)
-    }
-  });
+			if (entry.target.classList.contains("animation-group")) {
+				animationDelay = animationDelay + 100
+			} else if (entry.target.closest("animation-group")?.classList.contains("animated")) {
+				animationDelay = 0;
+			}
+			setTimeout(() => {
+				entry.target.classList.add("animated")
+			}, animationDelay);
+			observer.unobserve(entry.target)
+		}
+	});
 }, {
-  threshold: 0.3,
-  rootMargin: '50px',
+	threshold: 0.1,
+	rootMargin: '0px',
 });
 
 const animatedItems = document.querySelectorAll(".to_animate")
 
 if (animatedItems.length > 0) {
-  animatedItems.forEach(item => {
-    observer.observe(item)
-  })
+	animatedItems.forEach(item => {
+		observer.observe(item)
+	})
 }
+
+//Слайдер с целями
+advantagesSwiper = new Swiper(document.querySelector('.target__swiper'), {
+	direction: 'horizontal',
+	slidesPerView: 1.1,
+	grabCursor: true,
+	spaceBetween: 10,
+	breakpoints: {
+		510: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		},
+		1024: {
+			slidesPerView: 3,
+			spaceBetween: 30,
+		},
+	}
+}
+);
