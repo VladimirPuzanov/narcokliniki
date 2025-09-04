f4fad66b2cac5b3ffcb2de68f1c2a391c1426673
Vladimir <vladimir@mail.tam>
2025-09-04 16:38:36 +0300

    Начал попап - карту

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -392,6 +392,7 @@ ctabsTabSwiper = new Swiper(document.querySelector(".ctabs__mini"), {
 })
 
 ctabsContentSwiper = new Swiper(document.querySelector(".ctabs__content"), {
+	spaceBetween: 5,
 	thumbs: {
 		swiper: ctabsTabSwiper,
 		autoHeight: true,
@@ -403,6 +404,10 @@ ctabsContentSwiper = new Swiper(document.querySelector(".ctabs__content"), {
 cLicense = new Swiper(document.querySelector(".c-license__col_license.swiper"), {
 	spaceBetween: 15,
 	slidesPerView: 1.1,
+	navigation: {
+		nextEl: document.querySelector(".c-license__button_next"),
+		prevEl: document.querySelector(".c-license__button_back")
+	},
 	pagination: {
 		el: ".c-license__pagination"
 	},
@@ -414,10 +419,105 @@ cLicense = new Swiper(document.querySelector(".c-license__col_license.swiper"), 
 	}
 })
 
-cPhoto = new Swiper(document.querySelector(".ctabs-photo__swiper"), {
-	spaceBetween: 10,
-	slidesPerView: 1.1,
-	pagination: {
-		el: ".ctabs-photo__pagination"
-	},
-})
+function toggleClassOnScroll(element, topFixed) {
+    const parent = element.parentElement;
+		let bottomLine = 0
+		let isTopFixed = false
+		let isBottomFixed = false
+
+    function checkPosition() {
+			const elementRect = element.getBoundingClientRect()
+			const parentRect = parent.getBoundingClientRect()
+			if (elementRect.top <= parentRect.top && isTopFixed) {
+					element.classList.remove("top-fixed")
+					isTopFixed = false
+			}
+			if (elementRect.top <= topFixed && !isTopFixed && !isBottomFixed) {
+					element.classList.add("top-fixed")
+					isTopFixed = true
+			}
+			if(elementRect.bottom >= parentRect.bottom){
+					element.classList.remove("top-fixed")
+					element.classList.add("bottom-fixed")
+					isTopFixed = false
+					isBottomFixed = true
+					bottomLine = parentRect.bottom
+			}
+			if(isBottomFixed && bottomLine < parentRect.bottom){
+					element.classList.remove("bottom-fixed")
+					isBottomFixed = false
+			}
+    }
+
+    window.addEventListener('scroll', checkPosition);
+    checkPosition();
+}
+
+const fixedCol = document.querySelector(".fixed-col")
+if(fixedCol){
+	toggleClassOnScroll(fixedCol, 120)
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	function popupClose(popupActive) {
+		popupActive.classList.remove('open');
+		setTimeout(() => {
+			popupActive.classList.contains("open") || popupActive.classList.remove("active");
+		}, 400);
+		document.body.classList.remove('lock');
+		document.querySelector('html').style.paddingRight = 0;
+		document.querySelector('html').classList.remove('lock');
+		document.querySelector('header').removeAttribute('style');
+	}
+	const popupOpenBtns = document.querySelectorAll('.popup-btn');
+	const popups = document.querySelectorAll('.popup');
+	const closePopupBtns = document.querySelectorAll('.close-popup');
+	closePopupBtns.forEach(function (el) {
+		el.addEventListener('click', function (e) {
+			popupClose(e.target.closest('.popup'));
+		});
+	});
+	if (popups.length > 0) {
+		popups.forEach(function (popup) {
+			popupClose(popup);
+			popup.addEventListener('click', function (e) {
+				if (!e.target.closest('.popup__content')) {
+
+					popupClose(e.target.closest('.popup'));
+				}
+			});
+		});
+	}
+	popupOpenBtns.forEach(function (el) {
+		el.addEventListener('click', function (e) {
+			e.preventDefault();
+			const path = e.currentTarget.dataset.path;
+			const currentPopup = document.querySelector(`[data-target="${path}"]`);
+			if (currentPopup) {
+				currentPopup.classList.add('active');
+				setTimeout(() => {
+					currentPopup.classList.add("open");
+				}, 10);
+				if (currentPopup.getAttribute("data-target") == 'popup-change') {
+					let currentItem = el.closest('.change-item');
+					let originalTop = currentPopup.querySelector('.original-title');
+					let title = currentItem.querySelector('.change-title');
+					let subtitle = currentItem.querySelector('.change-subtitle');
+					if (title && subtitle) {
+						var newTitle = title.innerHTML + ' ' + subtitle.innerHTML;
+					} else if (title) {
+						var newTitle = title.innerHTML;
+					} else {
+						var newTitle = subtitle.innerHTML;
+					}
+					if (el.classList.contains('change-doctor')) {
+						newTitle = 'Записаться на приём к врачу: ' + newTitle;
+					}
+					originalTop.innerHTML = newTitle;
+				};
+				// scrollWidthFunc();
+				document.querySelector('html').classList.add('lock');
+			}
+		});
+	});
+});
